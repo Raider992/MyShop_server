e6159ba4541d84db6870666c17f6f8399c9563be
Raider992 <19stargazer92@gmail.com>
2021-02-09 18:04:16 +0300

    updated gitignore

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -90,6 +90,8 @@ if DEBUG:
        'template_profiler_panel.panels.template.TemplateProfilerPanel',
    ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 ROOT_URLCONF = 'my_shop.urls'
 
 TEMPLATES = [
@@ -174,9 +176,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
